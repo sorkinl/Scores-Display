d4cde7ff1f74f1249fc582bb2751c297bff40810
Leo Sorkin <50772160+sorkinl@users.noreply.github.com>
2020-01-31 10:29:18 -0500

    new component

--- src/Components/DisplayMatch.js
+++ src/Components/DisplayMatch.js
@@ -4,6 +4,12 @@ import {View, StyleSheet, Text} from 'react-native';
 const DisplayMatch = () => {
 // Display a component that will show 2 players and score between them, that takes its info 
 //from submit function in creatematchform
+
+return(
+    <View>
+        
+    </View>
+)
 }
 
 const styles = StyleSheet.create({});
